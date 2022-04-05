7c4cdbfaa8c567fd77ed64eb869cd0ab6d19bc4f
paaanic <speshilovmax@gmail.com>
2022-04-05 20:28:33 +0300

    Changed friend add view; added login required to views; changed template names

--- friendships/views.py
+++ friendships/views.py
@@ -1,22 +1,22 @@
-from audioop import reverse
 from django.contrib.auth import get_user_model
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.db.models import Q
+from django.forms import Form
 from django.http import HttpResponseRedirect
-from django.shortcuts import get_object_or_404
 from django.urls import reverse_lazy
-from django.views.generic import ListView, TemplateView
-from django.views.generic.detail import BaseDetailView, DetailView, SingleObjectTemplateResponseMixin
-from django.views.generic.edit import CreateView, DeleteView, FormMixin
+from django.views.generic import ListView
+from django.views.generic.detail import BaseDetailView, SingleObjectTemplateResponseMixin
+from django.views.generic.edit import DeleteView, FormMixin, ProcessFormView
 
 from . import manager as friendships_manager
+from .forms import FriendshipRequestForm
+from .mixins import UserIsFriendshipRequestReceiverTestMixin
 from .models import Friend, FriendshipRequest
 
 
 user_model = get_user_model()
 
 
-class FriendListView(ListView):
+class FriendListView(LoginRequiredMixin, ListView):
     template_name = 'friendships/list.html'
 
     def get_queryset(self):
@@ -26,43 +26,56 @@ class FriendListView(ListView):
         )
 
 
-class FriendAddView(TemplateView):
+class FriendshipRequestCreateView(
+    LoginRequiredMixin,
+    SingleObjectTemplateResponseMixin,
+    FormMixin,
+    ProcessFormView
+):
+    model = FriendshipRequest
+    form_class = FriendshipRequestForm
     template_name = 'friendships/add.html'
-    exception = None
+    success_url = reverse_lazy('friendships:sent_friendship_request_list')
+        
+    def get(self, request, *args, **kwargs):
+        self.object = None
+        return self.render_to_response(self.get_context_data())
 
-    def post(self, username):
-        to_user = get_object_or_404(user_model, username=username)
-        try:
-            friendships_manager.add_friend(self.request.user, to_user)
-        except (Friend.AlreadyExists, FriendshipRequest.AlreadyExists) as exc:
-            self.exception = exc.__qualname__.lower()
+    def post(self, request, *args, **kwargs):
+        self.object = None
+        form = self.get_form()
+        if form.is_valid():
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        if self.exception is not None:
-            context['exception'] = self.exception
-        return context
-
-
-class FriendDeleteView(DeleteView):
-    template_name = 'friendships/delete.html'
-    success_url = reverse_lazy('friendships:list')
-
-    def get_queryset(self):
-        return Friend.objects.filter(
-            from_user=self.request.user,
-            to_user=self.request.kwargs['username']
-        )
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs.update({'user': self.request.user})
+        return kwargs
 
     def form_valid(self, form):
-        success_url = self.get_success_url()
-        to_user = self.request.kwargs['username']
-        friendships_manager.remove_friend(self.request.user, to_user)
-        return HttpResponseRedirect(success_url)
+        to_username = form.cleaned_data.get('to_username')
+        try:
+            to_user = user_model.objects.get(username=to_username)
+            friendships_manager.add_friend(self.request.user, to_user)
+        except user_model.DoesNotExist:
+            pass
+        return HttpResponseRedirect(self.get_success_url())
+
+
+class FriendDeleteView(LoginRequiredMixin, DeleteView):
+    template_name = 'friendships/delete.html'
+    success_url = reverse_lazy('friendships:list')
+    slug_field = 'to_user__username'
+    slug_url_kwarg = 'username'
+
+    def get_queryset(self):
+        return Friend.objects.filter(from_user=self.request.user)
     
 
-class FriendshipRequestListView(ListView):
-    template_name = 'friendships/friendship_requests.html'
+class FriendshipRequestListView(LoginRequiredMixin, ListView):
+    template_name = 'friendships/friendship_request_list.html'
 
     def get_queryset(self):
         return (
@@ -71,12 +84,12 @@ class FriendshipRequestListView(ListView):
         )
 
 
-class SentFriendshipRequestListView(ListView):
-    template_name = 'friendships/sent_friendship_requests.html'
+class SentFriendshipRequestListView(LoginRequiredMixin, ListView):
+    template_name = 'friendships/sent_friendship_request_list.html'
 
     def get_queryset(self):
         return (
-            friendships_manager.requests(self.request.user)
+            friendships_manager.sent_requests(self.request.user)
             .order_by('-create_date')
         )
 
@@ -84,10 +97,12 @@ class SentFriendshipRequestListView(ListView):
 class BaseFriendshipRequestActionView(
     SingleObjectTemplateResponseMixin, FormMixin, BaseDetailView
 ):
+    model = FriendshipRequest
+    form_class = Form
     success_url = reverse_lazy('friendships:list')
 
     def action(self, friendship_request):
-        raise NotImplementedError("Provide action method for handling frienship request")
+        raise NotImplementedError("Provide action method for handling friendship request")
 
     def post(self, request, *args, **kwargs):
         self.object = self.get_object()
@@ -103,15 +118,23 @@ class BaseFriendshipRequestActionView(
         return HttpResponseRedirect(success_url)
 
 
-class FriendshipRequestAcceptView(BaseFriendshipRequestActionView):
+class FriendshipRequestAcceptView(
+    LoginRequiredMixin,
+    UserIsFriendshipRequestReceiverTestMixin,
+    BaseFriendshipRequestActionView
+):
     template_name = 'friendships/accept_friendship_request.html'
 
     def action(self, friendship_request):
         friendship_request.accept()
 
 
-class FriendshipRequestRejectView(BaseFriendshipRequestActionView):
-    template_name = 'friendships/accept_friendship_request.html'
+class FriendshipRequestRejectView(
+    LoginRequiredMixin,
+    UserIsFriendshipRequestReceiverTestMixin,
+    BaseFriendshipRequestActionView
+):
+    template_name = 'friendships/reject_friendship_request.html'
     success_url = reverse_lazy('friendships:list')
 
     def action(self, friendship_request):
