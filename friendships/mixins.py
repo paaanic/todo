fa9c5297e88ad4161cd2f03bb4990e3976f49253
paaanic <speshilovmax@gmail.com>
2022-04-05 18:58:58 +0300

    Added mixin for testing if user is a friendship request receiver

--- friendships/mixins.py
+++ friendships/mixins.py
@@ -1,1 +1,7 @@
 from django.contrib.auth.mixins import UserPassesTestMixin
+
+
+class UserIsFriendshipRequestReceiverTestMixin(UserPassesTestMixin):
+    def test_func(self):
+        obj = self.get_object()
+        return self.request.user == obj.to_user
