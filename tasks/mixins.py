7feef94a6cad30ceb9f4894311a9ed8ca65aa9f1
paaanic <speshilovmax@gmail.com>
2022-04-11 14:41:23 +0300

    Deleted notification user test mixin

--- tasks/mixins.py
+++ tasks/mixins.py
@@ -5,9 +5,3 @@ class UserIsTaskAuthorTestMixin(UserPassesTestMixin):
     def test_func(self):
         obj = self.get_object()
         return obj.author == self.request.user
-
-
-class UserIsTaskAuthorNotificationTestMixin(UserPassesTestMixin):
-    def test_func(self):
-        obj = self.get_object()
-        return obj.task.author == self.request.user
