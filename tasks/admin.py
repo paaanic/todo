fee6fa3ee3017e5395e7e7f4c440e6aa28296ee4
paaanic <speshilovmax@gmail.com>
2022-04-11 14:42:16 +0300

    Deleted task notification model registering to admin site

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,18 +1,11 @@
 from django.contrib import admin
 
-from .models import Task, TaskNotification, TaskShare
-
-
-class TaskNotificationInline(admin.StackedInline):
-    model = TaskNotification
-    extra = 0
+from .models import Task, TaskShare
 
 
 class TaskAdmin(admin.ModelAdmin):
-    inlines = [TaskNotificationInline]
     readonly_fields = ['create_date']
 
 
 admin.site.register(Task, TaskAdmin)
-admin.site.register(TaskNotification)
 admin.site.register(TaskShare)
