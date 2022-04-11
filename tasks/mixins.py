013646aac1cf94095a49421114f596a220c3eadd
paaanic <speshilovmax@gmail.com>
2022-04-11 18:58:59 +0300

    Added UserIsTaskShareToUserTestMixin

--- tasks/mixins.py
+++ tasks/mixins.py
@@ -5,3 +5,9 @@ class UserIsTaskAuthorTestMixin(UserPassesTestMixin):
     def test_func(self):
         obj = self.get_object()
         return obj.author == self.request.user
+        
+
+class UserIsTaskShareToUserTestMixin(UserPassesTestMixin):
+    def test_func(self):
+        task_share = self.get_object()
+        return task_share.to_user == self.request.user
