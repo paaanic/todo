75c444c16fae5418e52a15e02be7409febef4b52
paaanic <speshilovmax@gmail.com>
2022-04-21 01:15:56 +0300

    Updated tests for TelegramDispatcher

--- notifications/tests/test_dispatchers.py
+++ notifications/tests/test_dispatchers.py
@@ -1,17 +1,42 @@
+from unittest import mock
+
+from django.conf import settings
 from django.test import SimpleTestCase
 
 from ..dispatchers import BaseDispatcher, TelegramDispatcher
+from ..models import BaseNotification
 
 
 class BaseDispatcherTest(SimpleTestCase):
     def test_dispatch(self):
-        with self.assertRaises(TypeError):
+        with self.assertRaises(NotImplementedError):
             BaseDispatcher.dispatch(None)
 
 
-class TelegramDispatcher:
-    def test_dispatch(self):
-        pass
+class TelegramDispatcherTest(SimpleTestCase):
+    def setUp(self):
+        self.mock__send_message = mock.patch.object(
+            TelegramDispatcher, '_send_message'
+        )
 
+    def test_dispatch(self):
+        def _mock_init(self, verbose_name=None, **kwargs):
+            self.__dict__.update(kwargs)
+
+        mock_init = mock.patch.object(
+            BaseNotification, '__init__', _mock_init
+        )
+
+        with mock_init:
+            notif = BaseNotification(
+                dispatch_user_id='1', comment="Test comment"
+            )
+            with mock.patch.object(notif, 'delete'):
+                TelegramDispatcher.dispatch(notif)
+                notif.delete.assert_called_once()
+        
     def test_send_message(self):
-        pass
+        msg = "Test message"
+        with self.mock__send_message:
+            TelegramDispatcher._send_message(None, msg)
+            TelegramDispatcher._send_message.assert_called_once_with(None, msg)
