4f015522f50787704d7f7484bcf5ad1443c8cb69
paaanic <speshilovmax@gmail.com>
2022-04-21 17:35:15 +0300

    Set ALLOWED_HOSTS in config.settings

--- config/settings.py
+++ config/settings.py
@@ -30,8 +30,7 @@ SECRET_KEY = env.str('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', False)
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 # Application definition
 
