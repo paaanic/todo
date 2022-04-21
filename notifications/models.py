3449f334afd1568da61106b50e5c249f300786d6
paaanic <speshilovmax@gmail.com>
2022-04-21 17:38:47 +0300

    Delete __str__ method of BaseNotification model

--- notifications/models.py
+++ notifications/models.py
@@ -51,9 +51,6 @@ class BaseNotification(models.Model):
     class Meta:
         abstract = True
 
-    def __str__(self):
-        return f"Notification: {self.message}"
-
     def clean(self):
         if self.datetime <= timezone.now():
             raise ValidationError("Notification datetime cannot be in past")
